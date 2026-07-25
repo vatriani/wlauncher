164b6bde24b23ea466c162d75118c121d542fbf2
N. Neumann <vatriani.nn@googlemail.com>
2026-07-25 21:54:24 +0200

    WAYLAND-PROTOKOLL fix removes hardcoded versions

--- src/wayland-core.c
+++ src/wayland-core.c
@@ -209,11 +209,11 @@ static const struct wl_seat_listener seat_listener = {
 
 void registry_handle_global(void *data, struct wl_registry *registry, uint32_t id, const char *interface, uint32_t version) {
     register struct app_context *ctx = (struct app_context *)data;
-    (void)version;
+
     if (strncmp(interface, "wl_compositor", 13) == 0) {
-        ctx->compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 4);
+      ctx->compositor = wl_registry_bind(registry, id, &wl_compositor_interface, version);
     } else if (strncmp(interface, "zwlr_layer_shell_v1", 19) == 0) {
-        ctx->layer_shell = wl_registry_bind(registry, id, &zwlr_layer_shell_v1_interface, 4);
+        ctx->layer_shell = wl_registry_bind(registry, id, &zwlr_layer_shell_v1_interface, version);
     } else if (strncmp(interface, "wl_shm", 6) == 0) {
         ctx->shm = wl_registry_bind(registry, id, &wl_shm_interface, 1);
     } else if (strncmp(interface, "wl_seat", 7) == 0) {
